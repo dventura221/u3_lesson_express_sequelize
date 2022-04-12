4f5cc3b016149c1b5302a717a33b1e21d9635b74
Dan Ventura <danice.ventura@gmail.com>
2022-04-12 09:36:19 -0600

    complete

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,7 +1,59 @@
 const { Comment, User } = require('../models')
 
 // Work Here
+const GetCommentDetails = async (req, res) => {
+  try {
+    const comment = await Comment.findByPk(req.params.comment_id)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreateComment = async (req, res) => {
+  try {
+    let ownerId = parseInt(req.params.user_id)
+    let twertId = parseInt(req.params.twert_id)
+    let commentBody = {
+      ownerId,
+      twertId,
+      ...req.body
+    }
+    let comment = await Comment.create(commentBody)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    let updatedComment = await Comment.update(req.body, {
+      where: { id: commentId },
+      returning: true
+    })
+    res.send(updatedComment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    await Comment.destroy({ where: { id: commentId } })
+    res.send({ message: `Deleted comment with an id of ${commentId}` })
+  } catch (error) {
+    throw error
+  }
+}
 // Work Here
 
 // Dont forget to export your functions
-module.exports = {}
+module.exports = {
+  GetCommentDetails,
+  CreateComment,
+  UpdateComment,
+  DeleteComment
+}
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,6 +1,10 @@
 const Router = require('express').Router()
-
+const controller = require('../controllers/CommentController')
 // You Do
 // Implement Crud Operations For Comments
+Router.get('/view/:comment_id', controller.GetCommentDetails)
+Router.post('/:owner_id/:twert_id', controller.CreateComment)
+Router.put('/:comment_id', controller.UpdateComment)
+Router.delete('/:comment_id', controller.DeleteComment)
 // You Do
 module.exports = Router
